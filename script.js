16ca1e8db860a0e8f3dae8edb6b1c06054a99385
reynoldnjord <njordreynold@gmail.com>
2022-07-15 20:54:45 +0300

    Guided project from geeksforgeeks

--- script.js
+++ script.js
@@ -1,54 +1,129 @@
-let playerSelection;
-let computerSelection;
-let computerScore = 0;
-let playerScore = 0;
-
-function computerPlay() {
-    const randomIndex = Math.floor(Math.random() * 3);
-    switch (randomIndex){
-        case 0:
-            return "rock";
-        case 1:
-            return  "paper";
-        case 2:
-            return "scissor";
+const game = () => {
+    let playerScore = 0;
+    let computerScore = 0;
+    let moves = 0;
+ 
+ 
+    // Function to
+    const playGame = () => {
+        const rockBtn = document.querySelector('.rock');
+        const paperBtn = document.querySelector('.paper');
+        const scissorBtn = document.querySelector('.scissor');
+        const playerOptions = [rockBtn,paperBtn,scissorBtn];
+        const computerOptions = ['rock','paper','scissors']
+         
+        // Function to start playing game
+        playerOptions.forEach(option => {
+            option.addEventListener('click',function(){
+ 
+                const movesLeft = document.querySelector('.movesleft');
+                moves++;
+                movesLeft.innerText = `Moves Left: ${10-moves}`;
+                 
+ 
+                const choiceNumber = Math.floor(Math.random()*3);
+                const computerChoice = computerOptions[choiceNumber];
+ 
+                // Function to check who wins
+                winner(this.innerText,computerChoice)
+                 
+                // Calling gameOver function after 10 moves
+                if(moves == 10){
+                    gameOver(playerOptions,movesLeft);
+                }
+            })
+        })
+         
     }
+ 
+    // Function to decide winner
+    const winner = (player,computer) => {
+        const result = document.querySelector('.result');
+        const playerScoreBoard = document.querySelector('.p-count');
+        const computerScoreBoard = document.querySelector('.c-count');
+        player = player.toLowerCase();
+        computer = computer.toLowerCase();
+        if(player === computer){
+            result.textContent = 'Tie'
+        }
+        else if(player == 'rock'){
+            if(computer == 'paper'){
+                result.textContent = 'Computer Won';
+                computerScore++;
+                computerScoreBoard.textContent = computerScore;
+ 
+            }else{
+                result.textContent = 'Player Won'
+                playerScore++;
+                playerScoreBoard.textContent = playerScore;
+            }
+        }
+        else if(player == 'scissors'){
+            if(computer == 'rock'){
+                result.textContent = 'Computer Won';
+                computerScore++;
+                computerScoreBoard.textContent = computerScore;
+            }else{
+                result.textContent = 'Player Won';
+                playerScore++;
+                playerScoreBoard.textContent = playerScore;
+            }
+        }
+        else if(player == 'paper'){
+            if(computer == 'scissors'){
+                result.textContent = 'Computer Won';
+                computerScore++;
+                computerScoreBoard.textContent = computerScore;
+            }else{
+                result.textContent = 'Player Won';
+                playerScore++;
+                playerScoreBoard.textContent = playerScore;
+            }
+        }
+    }
+ 
+    // Function to run when game is over
+    const gameOver = (playerOptions,movesLeft) => {
+ 
+        const chooseMove = document.querySelector('.move');
+        const result = document.querySelector('.result');
+        const reloadBtn = document.querySelector('.reload');
+ 
+        playerOptions.forEach(option => {
+            option.style.display = 'none';
+        })
+ 
+      
+        chooseMove.innerText = 'Game Over!!'
+        movesLeft.style.display = 'none';
+ 
+        if(playerScore > computerScore){
+            result.style.fontSize = '2rem';
+            result.innerText = 'You Won The Game'
+            result.style.color = '#308D46';
+        }
+        else if(playerScore < computerScore){
+            result.style.fontSize = '2rem';
+            result.innerText = 'You Lost The Game';
+            result.style.color = 'red';
+        }
+        else{
+            result.style.fontSize = '2rem';
+            result.innerText = 'Tie';
+            result.style.color = 'grey'
+        }
+        reloadBtn.innerText = 'Restart';
+        reloadBtn.style.display = 'flex'
+        reloadBtn.addEventListener('click',() => {
+            window.location.reload();
+        })
+    }
+ 
+ 
+    // Calling playGame function inside game
+    playGame();
+     
 }
-
-function playRound(playerSelection, computerSelection) {
-    computerSelection = computerPlay();
-    playerSelection = playerSelection.toLowerCase();
-    if ((playerSelection==="rock" && computerSelection==="paper") ||
-    (playerSelection==="paper" && computerSelection==="scissor") ||
-    (playerSelection==="scissor" && computerSelection==="rock")){
-        alert("You Lose!");
-        computerScore++;
-    }
-
-    if ((playerSelection==="rock" && computerSelection==="scissor") ||
-    (playerSelection==="paper" && computerSelection==="rock") ||
-    (playerSelection==="scissor" && computerSelection==="paper")){
-        alert("You Win!");
-        playerScore++
-    }
-
-    if (playerSelection === computerSelection) {
-        return String("It's a tie!");
-    }
-}
-
-// creating the UI
-const buttons = document.querySelectorAll('button');
-const score = document.createElement('div');
-const buttonDiv = document.getElementById('but-div');
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        let text = button.textContent;
-        playerSelection = text.toLowerCase;
-        playRound(playerSelection, computerSelection);
-        score.innerHTML += "Computer's Score: ${computerScore} <br> Player's Score: ${playerScore}";
-        score.setAttribute('#score');
-        buttonDiv.appendChild(score);
-    })
-})
-
+ 
+// Calling the game function
+game();
